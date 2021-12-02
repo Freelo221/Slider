3de45aa1f7ab67987dbd742b8abb0a3524adffa0
Freelo221 <Jannis_Philipp@web.de>
2021-12-02 14:07:56 +0100

    New

--- rangeslider.js
+++ rangeslider.js
@@ -1,21 +1,23 @@
-export class RangeSlider {
+class RangeSlider {
 
+    constructor(base) {
+        this.base = base;
+        const baseElement = document.querySelector("#" + base.element);
+        const outputElement = document.querySelector("#" + base.output);
 
+        base.min ? this.min = base.min : this.min = 0;
+        base.max ? this.max = base.max : this.max = 5;
+        base.step ? this.step = base.step : this.step = 1;
+        base.start ? this.startPos = base.start : this.startPos = 2;
 
+        base.unit ? this.unit = base.unit : this.unit = "";
 
-    constructor(element, color, ...test) {
-        // this.elem = element;
-        // this.color = color;
-        this.dragItem = "";
-        this.container = "";
+        base.highlightColor ? this.highlightColor = base.highlightColor : this.highlightColor = "red";
+        base.highlight ? this.highlight = base.highlight : this.highlight = false;
+        if (base.SliderDots) {
+            base.SliderDots.color ? this.DotColor = base.SliderDots.color : this.DotColor = "#ccc";
+        }
 
-        this.min = 0;
-        this.max = 0;
-        this.step = 0;
-
-
-        this.dragItemHeight = 0;
-        this.dragItemWidth = 0;
         this.currentPos = 0;
         this.active = false;
         this.currentX = 0;
@@ -27,10 +29,174 @@ export class RangeSlider {
         this.pointWidth = 0;
         this.posAll = [];
         this.valuesall = [];
+        this.dotList = [];
+
+
+        //Slider Container
+        base.color ? baseElement.style.backgroundColor = base.color : baseElement.style.backgroundColor = "#ecedef"
+        base.width ? baseElement.style.width = base.width : baseElement.style.width = "500px";
+        base.height ? baseElement.style.height = base.height : baseElement.style.height = "30px";
+
+
+
+
+        //Slider Track Element
+        let sliderTrack = document.createElement('div');
+        sliderTrack.classList.add("track");
+        if (base.SliderTrack) {
+            sliderTrack.style.height = base.SliderTrack.height;
+            sliderTrack.style.background = base.SliderTrack.background;
+        }
+        baseElement.appendChild(sliderTrack);
+
+
+
+        //Slider Front Element
+        let sliderFront = document.createElement('div');
+        sliderFront.classList.add("sliderFront");
+        if (base.FrontItem) {
+            if (!base.FrontItem.show) {
+                sliderFront.style.display = "none";
+            } else {
+                sliderFront.style.width = base.FrontItem.width;
+                sliderFront.style.left = base.FrontItem.offset;
+                sliderFront.style.background = base.FrontItem.color;
+            }
+        }
+
+        baseElement.appendChild(sliderFront);
+
+
+        //Slider Back Element
+        let sliderBack = document.createElement('div');
+        sliderBack.classList.add("sliderBack");
+        if (base.BackItem) {
+            if (!base.BackItem.show) {
+                sliderBack.style.display = "none";
+            } else {
+                sliderBack.style.width = base.BackItem.width;
+                sliderBack.style.right = base.BackItem.offset;
+                sliderBack.style.background = base.BackItem.color;
+            }
+        }
+
+        baseElement.appendChild(sliderBack);
+
+        //Slider Drag Element
+        let sliderHandle = document.createElement('div');
+        sliderHandle.classList.add("SliderHandle");
+        sliderHandle.setAttribute("id", "item");
+        if (base.SliderHandle) {
+            sliderHandle.style.height = base.SliderHandle.dragItemHeight;
+            sliderHandle.style.width = base.SliderHandle.dragItemWidth;
+            sliderHandle.style.border = base.SliderHandle.borderWidth + " solid " + base.SliderHandle.borderColor;
+        }
+
+        baseElement.appendChild(sliderHandle);
+
+
+        this.container = baseElement;
+        this.output = outputElement;
+        this.dragItem = sliderHandle;
+
+        this.trackItem = sliderTrack;
+        this.frontItem = sliderFront;
+        this.backItem = sliderBack;
+
+        this.dragItemHeight = this.dragItem.offsetHeight;
+        this.dragItemWidth = this.dragItem.offsetWidth;
+
+        this.currentPos = 0;
+        this.active = false;
+        this.xOffset = 0;
+        this.yOffset = 0;
+
+        let count = (this.max - this.min) / this.step;
+
+        for (var i = 0; i <= count; i++) {
+
+            var pos = (100 / ((this.max - this.min) / this.step)) * i;
+            this.posAll.push(pos);
+            this.valuesall.push(i * this.step);
+            if (i == 0) {
+                let elem = document.createElement('div');
+                elem.classList.add('sliderBreak');
+                elem.setAttribute('style', 'left: calc(' + pos + '%'); //mb add some offset
+                if (base.SliderDots) {
+                    elem.style.scale = base.SliderDots.scale;
+                    elem.style.background = base.SliderDots.color;
+                    if (base.SliderDots.form == "square") {
+                        elem.style.borderRadius = "0%";
+                    } else {
+
+                    }
+                }
+                baseElement.appendChild(elem);
+                this.dotList.push(elem);
+            } else if (i == count) {
+                let elem = document.createElement('div');
+                elem.classList.add('sliderBreak');
+                elem.setAttribute('style', 'left: calc(' + pos + '%'); //mb add some offset
+                if (base.SliderDots) {
+                    elem.style.scale = base.SliderDots.scale;
+                    elem.style.background = base.SliderDots.color;
+                    if (base.SliderDots.form == "square") {
+                        elem.style.borderRadius = "0%";
+                    } else {
+
+                    }
+                }
+                baseElement.appendChild(elem);
+                this.dotList.push(elem);
+            } else {
+                let elem = document.createElement('div');
+                elem.classList.add('sliderBreak');
+                elem.setAttribute('style', 'left: calc(' + pos + '%'); //mb add some offset
+                if (base.SliderDots) {
+                    elem.style.scale = base.SliderDots.scale;
+                    elem.style.background = base.SliderDots.color;
+                    if (base.SliderDots.form == "square") {
+                        elem.style.borderRadius = "0%";
+                    } else {
+
+                    }
+                }
+                baseElement.appendChild(elem);
+                this.dotList.push(elem);
+            }
+        }
+        this.pointWidth = document.querySelector("#" + base.element + " .sliderBreak").getBoundingClientRect().width;
+        this.SnapToGrid(this.container.offsetWidth * this.posAll[this.startPos] / 100);
+
+        var startDrag = this.dragStart.bind(this);
+        var endDrag = this.dragEnd.bind(this);
+        var mouseDrag = this.drag.bind(this);
+        var printMousePos = this.printMousePos.bind(this);
+        baseElement.addEventListener('click', printMousePos);
+
+        baseElement.addEventListener("touchstart", startDrag);
+        baseElement.addEventListener("touchend", endDrag);
+        baseElement.addEventListener("touchmove", mouseDrag);
+
+        baseElement.addEventListener("mousedown", startDrag);
+        baseElement.addEventListener("mouseup", endDrag);
+        baseElement.addEventListener("mousemove", mouseDrag);
+
+    }
+
+    printMousePos(e) {
+        console.log(this.active)
+        if (!this.isDrag) {
+            var rect = e.target.getBoundingClientRect();
+            var x = e.clientX - rect.left;
+            var y = e.clientY - rect.top;
+            this.SnapToGrid(x, 0, this.dragItem);
+        } else {
+            this.isDrag = false;
+        }
     }
 
     dragStart(e) {
-        console.log("object");
         if (e.type === "touchstart") {
             this.initialX = e.touches[0].clientX - this.xOffset;
             this.initialY = e.touches[0].clientY - this.yOffset;
@@ -38,82 +204,62 @@ export class RangeSlider {
             this.initialX = e.clientX - this.xOffset;
             this.initialY = e.clientY - this.yOffset;
         }
-        // alert(e.target);
-        // alert(this.dragItem);
+
         if (e.target === this.dragItem) {
-            alert();
             this.active = true;
         }
+
     }
 
-
     dragEnd(e) {
-
-
         if (this.currentX <= 0) {
             this.currentX = 0;
         }
-        // console.log(currentX);
-        // if (currentX >= 400) {
-        //     currentX = container.offsetWidth - 30;
-        // }
-        // this.SnapToGrid(this.currentX);
-        // setTranslate(currentX, 0, dragItem);
+        this.SnapToGrid(this.currentX);
         this.active = false;
     }
 
-
-
     drag(e) {
         if (this.active) {
-
+            this.isDrag = true;
             e.preventDefault();
 
             if (e.type === "touchmove") {
                 this.currentX = e.touches[0].clientX - this.initialX;
-                // console.log(currentX);
-                // currentY = e.touches[0].clientY - initialY;
             } else {
-                this.currentX = e.clientX - initialX;
-                console.log(this.currentX);
-                // currentY = e.clientY - initialY;
+                this.currentX = e.clientX - this.initialX;
             }
-
-
-            // yOffset = currentY;
-            $(".track").css("width", (this.container.offsetWidth - (Math.abs(this.currentX - this.container.offsetWidth)) + (this.dragItemWidth / 2)));
+            this.trackItem.style.width = this.container.offsetWidth - (Math.abs(this.currentX - this.container.offsetWidth)) + (this.dragItemWidth / 2) + "px";
             this.setTranslate(this.currentX, 0, this.dragItem);
         }
     }
 
 
     setTranslate(xPos, yPos, el) {
-        // initialX = xPos
-        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-        $(".track").css("width", (this.container.offsetWidth - (Math.abs(xPos - this.container.offsetWidth)) + (this.dragItemWidth / 2)));
-        console.log(parseInt(this.valuesall[this.currentPos]) + parseInt(this.min));
-        $("#SliderValueOutput").text(this.valuesall[this.currentPos] + parseInt(this.min));
+        this.dragItem.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+        this.trackItem.style.width = this.container.offsetWidth - (Math.abs(xPos - this.container.offsetWidth)) + (this.dragItemWidth / 2) + "px";
+        this.output.innerHTML = (this.valuesall[this.currentPos] + parseInt(this.min) + this.unit);
+        this.HighlightDot();
     }
 
     SnapToGrid(posX) {
 
         var finalPos = 0;
-        alert(this.posAll);
         this.posAll.forEach((elem, i) => {
             let curPoint = this.container.offsetWidth * (elem / 100);
             if (posX > curPoint) {
                 if (i != this.posAll.length) {
-                    let offsetPrevPoint = posX - ((this.posAll[i] / 100) * container.offsetWidth);
-                    console.log("offset1: " + offsetPrevPoint);
-                    let offsetNextPoint = ((this.posAll[i + 1] / 100) * container.offsetWidth) - posX;
-                    console.log("offset2: " + offsetNextPoint);
+                    let offsetPrevPoint = posX - ((this.posAll[i] / 100) * this.container.offsetWidth);
+                    let offsetNextPoint = ((this.posAll[i + 1] / 100) * this.container.offsetWidth) - posX;
+
                     if (offsetNextPoint > offsetPrevPoint) {
                         if (i == 0) {
-                            finalPos = ((0 - (dragItemWidth / 2)) + 0) + 1
+                            finalPos = ((0 - (this.dragItemWidth / 2)) + 0) + 1
                             this.currentPos = 0;
                             this.xOffset = finalPos;
+
                         } else {
-                            finalPos = (this.posAll[i] / 100) * container.offsetWidth - this.dragItemWidth / 2 + 1;
+                            finalPos = (this.posAll[i] / 100) * this.container.offsetWidth - this.dragItemWidth / 2 + 1;
                             this.currentPos = i;
                             this.xOffset = finalPos;
                         }
@@ -122,11 +268,10 @@ export class RangeSlider {
                         finalPos = (this.posAll[i + 1] / 100) * this.container.offsetWidth - this.dragItemWidth / 2 + 1;
                         this.currentPos = i + 1;
                         this.xOffset = finalPos + 0;
-                        if ((this.posAll[i + 1] / 100) * container.offsetWidth == 500) {
-                            // finalPos = ((posAll[i + 1] / 100) * container.offsetWidth - (this.dragItemWidth / 2) - pointWidth + 0 + 1);
+                        if ((this.posAll[i + 1] / 100) * this.container.offsetWidth == 500) {
                             finalPos = ((this.posAll[i + 1] / 100) * this.container.offsetWidth - (this.dragItemWidth / 2) + 0 + 1);
                             this.currentPos = i + 1;
-                            xOffset = finalPos + 0;
+                            this.xOffset = finalPos + 0;
                         }
                     }
 
@@ -136,109 +281,34 @@ export class RangeSlider {
             } else {
                 if (i == 0) {
                     finalPos = ((0 - (this.dragItemWidth / 2)) + 0 + 1)
-                    globalThis.currentPos = 0;
-                    alert(self.currentPos);
-                    globalThis.xOffset = finalPos;
-                    console.log("asdasd");
+                    this.currentPos = 0;
+                    this.xOffset = finalPos;
                 }
             }
 
         });
-        // $(this.posAll).each(function(i, elem) {
-
-        // })
-
-        // setTranslate(finalPos, 0, this.dragItem);
-        // this.initialX = finalPos;
+        this.initialX = finalPos;
+        this.setTranslate(finalPos, 0, this.dragItem);
     }
 
-    testfunction() {
-        alert(this.dragItem);
-    }
-
-    test(base, breakpoints) {
-        const baseElement = document.querySelector("#" + base.element);
-        const track = '<div class="track"></div>';
-        const sliderFront = '<div class="sliderFront"></div>';
-        const sliderBack = '<div class="sliderBack"></div>';
-        const handle = '<div class="item" id="item"></div>';
-        baseElement.innerHTML = track;
-        baseElement.innerHTML = sliderFront;
-        baseElement.innerHTML = sliderBack;
-        baseElement.innerHTML = handle;
-
-
-        this.dragItem = document.querySelector("#item");
-        alert(this.dragItem)
-        this.container = baseElement;
-
-        this.dragItemHeight = this.dragItem.offsetHeight;
-        this.dragItemWidth = this.dragItem.offsetWidth;
-
-        this.currentPos = 0;
-        this.active = false;
-        // var currentX;
-        // var currentY;
-        // var initialX;
-        // var initialY;
-        this.xOffset = 0;
-        this.yOffset = 0;
-
-        // var pointWidth;
-
-
-
-
-
-        // min = $("#container").attr("min");
-        let min = 5000;
-        this.min = min;
-        // max = $("#container").attr("max");
-        let max = 8000;
-        this.max = max;
-        // step = $("#container").attr("step");
-        let step = 1000;
-        this.step = step;
-        let count = (max - min) / step;
-
-        for (var i = 0; i <= count; i++) {
-
-            var pos = (100 / ((max - min) / step)) * i;
-            this.posAll.push(pos);
-            this.valuesall.push(i * step);
-            if (i == 0) {
-                console.log(i + ": first");
-                baseElement.innerHTML += "<div style='left: calc(" + pos + "%') class='sliderBreak'></div>";
-            } else if (i == count) {
-                console.log(i + ": last");
-                baseElement.innerHTML += "<div style='left: calc(" + pos + "% - 0px') class='sliderBreak'></div>";
-            } else {
-                console.log(i + ": mid");
-                baseElement.innerHTML += "<div style='left: calc(" + pos + "% - 0px') class='sliderBreak'></div>";
+    HighlightDot() {
+        this.dotList.forEach((elem, i) => {
+            if (this.highlight) {
+                elem.style.background = this.DotColor;
             }
+            elem.classList.remove("active");
+
+            // 
+
+
+        })
+
+        if (this.highlight) {
+            this.dotList[this.currentPos].style.background = this.highlightColor;
         }
-        // this.testa();
-        this.testfunction();
-        this.pointWidth = $(".sliderBreak")[0].getBoundingClientRect().height;
-        this.SnapToGrid(this.container.offsetWidth / 3);
-        console.log(globalThis.posAll);
-        // baseElement.insertAdjacentHTML(track)
-        document.getElementById(base.element).style.backgroundColor = base.color;
+        this.dotList[this.currentPos].classList.add("active");
+
+
+
     }
-
-
-}
-
-export function SetFunctions() {
-
-}
-
-
-
-function printMousePos(e) {
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
-    // SnapToGrid(x, 0, dragItem);
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
 }
